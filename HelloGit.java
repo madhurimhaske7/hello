16e964aee2bc34173ccfa529348034e60192c5a7
madhurimhaske7 <57608264+madhurimhaske7@users.noreply.github.com>
2022-11-27 21:21:22 +0530

    Update HelloGit.java

--- HelloGit.java
+++ HelloGit.java
@@ -13,6 +13,6 @@ public class HelloGit {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello GIT Jenkins");
+        System.out.print("Hello GIT Jenkins");
     }
 }
